cf5bf8b50a40a91ae3c75f0ab5a979574d9d0633
tsukudanin <86942635+tsukudanin@users.noreply.github.com>
2021-07-05 23:55:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,53 +1,26 @@
-import discord
-import random  # おみくじで使用
+from discord.ext import commands
+import os
+import traceback
 
-client = discord.Client()  # 接続に使用するオブジェクト
-
-
-@client.event
-async def on_ready():
-    """起動時に通知してくれる処理"""
-    print('ログインしました')
-    print(client.user.name)  # ボットの名前
-    print(client.user.id)  # ボットのID
-    print(discord.__version__)  # discord.pyのバージョン
-    print('------')
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@client.event
-async def on_message(message):
-    """メッセージを処理"""
-    if message.author.bot:  # ボットのメッセージをハネる
-        return
-
-    if message.content == "!眠たい":
-        # チャンネルへメッセージを送信
-        await message.channel.send(f"{message.author.mention}さん 寝ましょう")  # f文字列（フォーマット済み文字列リテラル）
-
-    elif message.content == "!おはよう":
-        # チャンネルへメッセージを送信
-        await message.channel.send(f"{message.author.mention}さん おはようございます")  # f文字列（フォーマット済み文字列リテラル）
-
-    elif message.content == "!投票":
-        # リアクションアイコンを付けたい
-        q = await message.channel.send("あなたは右利きですか？")
-        [await q.add_reaction(i) for i in ('⭕', '❌')]  # for文の内包表記
-
-    elif message.content == "!おみくじ":
-        # Embedを使ったメッセージ送信 と ランダムで要素を選択
-        embed = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢は！",
-                              color=0x2ECC69)
-        embed.set_thumbnail(url=message.author.avatar_url)
-        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '吉', '凶', '大凶')), inline=False)
-        await message.channel.send(embed=embed)
-
-    elif message.content == "!ダイレクトメッセージ":
-        # ダイレクトメッセージ送信
-        dm = await message.author.create_dm()
-        await dm.send(f"{message.author.mention}さんにダイレクトメッセージ")
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+    
+@bot.command()
+async def neko(ctx):
+    await ctx.send('にゃーん')
 
-# botの接続と起動
-# （botアカウントのアクセストークンを入れてください）
-client.run("ODQxMjI4NTE1NDI4NTMyMjU2.YJjtGw.5uMYBd-txUACFFYAGKVpwOX5yRg")
+
+        
+bot.run(token)
