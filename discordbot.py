4d6332639501de0486f73272704aebccefc65023
tsukudanin <86942635+tsukudanin@users.noreply.github.com>
2021-07-05 14:43:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,5 +21,36 @@ async def ping(ctx):
 async def neko(ctx):
     await ctx.send('にゃーん')
 
+@bot.event
+async def on_message(message):
+    """メッセージを処理"""
+    if message.author.bot:  # ボットのメッセージをハネる
+        return
+
+    if message.content == "!眠たい":
+        # チャンネルへメッセージを送信
+        await message.channel.send(f"{message.author.mention}さん 寝ましょう")  # f文字列（フォーマット済み文字列リテラル）
+
+    elif message.content == "!おはよう":
+        # チャンネルへメッセージを送信
+        await message.channel.send(f"{message.author.mention}さん おはようございます")  # f文字列（フォーマット済み文字列リテラル）
+
+    elif message.content == "!投票":
+        # リアクションアイコンを付けたい
+        q = await message.channel.send("あなたは右利きですか？")
+        [await q.add_reaction(i) for i in ('⭕', '❌')]  # for文の内包表記
+
+    elif message.content == "!おみくじ":
+        # Embedを使ったメッセージ送信 と ランダムで要素を選択
+        embed = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢は！",
+                              color=0x2ECC69)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '吉', '凶', '大凶')), inline=False)
+        await message.channel.send(embed=embed)
+
+    elif message.content == "!ダイレクトメッセージ":
+        # ダイレクトメッセージ送信
+        dm = await message.author.create_dm()
+        await dm.send(f"{message.author.mention}さんにダイレクトメッセージ")    
 
 bot.run(token)
