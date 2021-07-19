c87f14a0ee5675f41379b974eac7a1de8dacebf9
tsukudanin <86942635+tsukudanin@users.noreply.github.com>
2021-07-19 15:24:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -43,17 +43,17 @@ async def vote2(ctx):
 async def on_message(message):
 
     #メッセージを送信する
-    if message.content.startswith('/send_dm '):
+    if message.content.startswith('$send_dm '):
 
         # DMに送るメッセージを取得
-        msg = message.content.replace('/send_dm ', '')
+        msg = message.content.replace('$send_dm ', '')
 
         # コマンドを打った人のDMに送信
         await message.author.send(msg)
         # message.author はメッセージを送信したユーザーです
     
     # Embedメッセージを送信する
-    elif message.content.startswith('/send_embed'):
+    elif message.content.startswith('$send_embed'):
 
         # Embedメッセージ
         embed_msg = discord.Embed(title="Embedメッセージ", description="正常に送信されました")
@@ -62,12 +62,12 @@ async def on_message(message):
         await message.author.send(embed=embed_msg)
     
     # ファイルを送信する
-    elif message.content.startswith('/send_file'):
+    elif message.content.startswith('$send_file'):
         
         # コマンドを打った人のDMに送信
         await message.author.send(content='ファイル送信', file=discord.File('test.txt'))
         # discord.File()には、送信したいファイルの名前やファイルパスを入れます。
-        # 例) 'image.jpg', './image/1.png', 'data.txt' など
+        # 例) 'image.jpg', '.$image/1.png', 'data.txt' など
     
 @bot.command()
 async def member(ctx, day: int):
