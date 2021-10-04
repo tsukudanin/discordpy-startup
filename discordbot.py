508916e815458c1521fa13990904c3a73577da85
tsukudanin <86942635+tsukudanin@users.noreply.github.com>
2021-10-04 11:47:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,10 @@
 from discord.ext import commands
 import os
 import traceback
+import gspread
+import datetime
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 bot = commands.Bot(command_prefix='$')
 token = os.environ['DISCORD_BOT_TOKEN']
